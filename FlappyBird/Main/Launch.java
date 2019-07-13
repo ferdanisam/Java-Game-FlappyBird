7612947a9124b2bcaa67513cdcd1918c4f07c0db
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-07-13 15:20:08 +0800

    Update Launch.java

--- FlappyBird/Main/Launch.java
+++ FlappyBird/Main/Launch.java
@@ -1,1 +1,46 @@
+package Main;
 
+import Asset.Background;
+import Asset.Bird;
+import Loader.ImageLoader;
+import Asset.Number;
+import Asset.Obstacle;
+
+public class Launch {
+	
+	private final String title="Flappy Bird";
+	
+	private final int width=500;
+	
+	private final int height=800;
+	
+	public Window window;
+	
+	private Display display;
+	
+	private ImageLoader imageLoader;
+	
+	public static void main(String[]args) {
+		new Launch();
+	}
+	
+	public Launch() {
+		loadingData();
+		
+		window=new Window(title,width,height);
+		display=new Display(window);
+		display.start();
+	}
+	
+	private void loadingData() {
+		imageLoader=new ImageLoader();
+		
+		Background.initialize(imageLoader);
+		
+		Bird.initialize(imageLoader);
+		
+		Number.initialize(imageLoader);
+		
+		Obstacle.initialize(imageLoader);
+	}
+}
