d694a33c06aa87e3bc8f3d1c6becc14fc6fd8953
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-07-13 15:25:57 +0800

    Update State.java

--- FlappyBird/State/State.java
+++ FlappyBird/State/State.java
@@ -1,1 +1,27 @@
+package State;
 
+import java.awt.Graphics2D;
+
+import Main.Display;
+
+public abstract class State {
+	public static State currentState=null;
+	
+	public static void setState(State state) {
+		currentState=state;
+	}
+	
+	public static State getState() {
+		return currentState;
+	}
+	
+	protected Display display;
+	
+	public State(Display display) {
+		this.display=display;
+	}
+	
+	public abstract void tick();
+	
+	public abstract void render(Graphics2D g);
+}
