a8a7696ca9cf8eb17b378417198041a415b047e2
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-07-14 14:24:38 +0800

    Update ImageLoader.java

--- FlappyBird/Loader/ImageLoader.java
+++ FlappyBird/Loader/ImageLoader.java
@@ -1,1 +1,22 @@
+package Loader;
 
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
+public class ImageLoader {
+	public ImageLoader() {
+		
+	}
+	
+	public BufferedImage loadImage(String path) {
+		try {
+			return ImageIO.read(getClass().getClassLoader().getResource(path));
+		}catch(Exception exception) {
+			System.out.println("Cannot load image");
+		}
+		
+		return null;
+	}
+	
+}
