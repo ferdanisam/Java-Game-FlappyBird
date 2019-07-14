be631f94798f71501833379556bc0469dc9e7f22
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-07-14 12:55:24 +0800

    Update StartState.java

--- FlappyBird/State/StartState.java
+++ FlappyBird/State/StartState.java
@@ -1,1 +1,28 @@
+package State;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import Asset.Background;
+import Main.Display;
+
+public class StartState extends State {
+	
+	
+	public StartState(Display display) {
+		super(display);
+	}
+
+	
+	public void tick() {
+		if(display.getMouseActionHandler().getPressed()) {
+			State.setState(display.getGameState());
+		}
+	}
+	
+	public void render(Graphics2D g) {
+		g.setColor(Color.PINK);
+		g.fillRect(0,0,display.getWidth(),display.getHeight());
+		g.drawImage(Background.start,0,0,display.getWidth(),display.getHeight(),null);
+	}
+}
