839df945c357badc30f0a840e0b12376da8426c7
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-07-14 12:58:30 +0800

    Update EndState.java

--- FlappyBird/State/EndState.java
+++ FlappyBird/State/EndState.java
@@ -1,1 +1,28 @@
+package State;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import Main.Display;
+
+public class EndState extends State {
+	
+	private final BufferedImage over=Asset.Background.over;
+	
+	public EndState(Display display) {
+		super(display);
+	}
+	
+	public void tick() {
+		if(display.getMouseActionHandler().getPressed()) {
+			State.setState(display.getGameState());
+		}
+	}
+	
+	public void render(Graphics2D g) {
+		g.setColor(Color.PINK);
+		g.fillRect(0,0,display.getWidth(),display.getHeight());
+		g.drawImage(over,0,display.getHeight()/2-100,display.getWidth(),200,null);
+	}
+}
