f461fc9d34621b7bf9ce1573b1564588df892bad
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-07-14 14:26:26 +0800

    Update KeyActionHandler.java

--- FlappyBird/Handler/KeyActionHandler.java
+++ FlappyBird/Handler/KeyActionHandler.java
@@ -1,1 +1,33 @@
+package Handler;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyActionHandler implements KeyListener {
+
+	private boolean jump=false;
+	
+	
+	public void keyPressed(KeyEvent event) {
+		if(event.getKeyCode()==KeyEvent.VK_SPACE) {
+			jump=true;
+		}
+	}
+
+	public void keyReleased(KeyEvent event) {
+		
+	}
+	
+	public void keyTyped(KeyEvent event) {
+
+	}
+	
+	public void tick() {
+		jump=false;
+	}
+	
+	public boolean getJump() {
+		return jump;
+	}
+	
+}
