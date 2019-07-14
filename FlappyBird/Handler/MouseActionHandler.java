a7af79062f5b791c83b77252e8618ae21e684460
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-07-14 14:27:39 +0800

    Update MouseActionHandler.java

--- FlappyBird/Handler/MouseActionHandler.java
+++ FlappyBird/Handler/MouseActionHandler.java
@@ -1,1 +1,38 @@
+package Handler;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MouseActionHandler implements MouseListener {
+	
+	private boolean pressed=false;
+
+	public void mousePressed(MouseEvent event) {
+		pressed=true;
+	}
+
+	public void mouseReleased(MouseEvent event) {
+	
+	}
+	
+	public void mouseClicked(MouseEvent event) {
+		
+	}
+
+	public void mouseEntered(MouseEvent event) {
+		
+	}
+
+	public void mouseExited(MouseEvent event) {
+		
+	}
+	
+	public void tick() {
+		pressed=false;
+	}
+	
+	public boolean getPressed() {
+		return pressed;
+	}
+	
+}
