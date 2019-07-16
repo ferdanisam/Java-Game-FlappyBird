f2f04aaac6b46565a783fe0906c73938a4800502
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-07-16 11:56:05 +0800

    Update KeyActionHandler.java

--- src/Handler/KeyActionHandler.java
+++ src/Handler/KeyActionHandler.java
@@ -3,14 +3,33 @@ package Handler;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import Main.Display;
+
 public class KeyActionHandler implements KeyListener {
 
-	private boolean jump=false;
+	private Display display;
 	
+	private boolean jump=false;
+
+	private int pause=2;
+	
+	private boolean quit=false;
+	
+	public KeyActionHandler(Display display) {
+		this.display=display;
+	}
 	
 	public void keyPressed(KeyEvent event) {
 		if(event.getKeyCode()==KeyEvent.VK_SPACE) {
 			jump=true;
+		}
+		
+		if(event.getKeyCode()==KeyEvent.VK_P) {
+			pause++;
+		}
+		
+		if(event.getKeyCode()==KeyEvent.VK_ESCAPE) {
+			quit=true;
 		}
 	}
 
@@ -19,15 +38,24 @@ public class KeyActionHandler implements KeyListener {
 	}
 	
 	public void keyTyped(KeyEvent event) {
-
+		
 	}
 	
 	public void tick() {
 		jump=false;
+		
+		if(quit==true) {
+			display.stop();
+		}
+		
 	}
 	
 	public boolean getJump() {
 		return jump;
+	}
+	
+	public int getPause() {
+		return pause;
 	}
 	
 }
