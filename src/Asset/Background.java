d6f35294fc4647b4679edff44bea67013db82d2c
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-07-16 12:02:45 +0800

    Update Background.java

--- src/Asset/Background.java
+++ src/Asset/Background.java
@@ -1,1 +1,24 @@
+package Asset;
 
+import java.awt.image.BufferedImage;
+
+import Loader.ImageLoader;
+
+public class Background {
+	public static BufferedImage day;
+	
+	public static BufferedImage night;
+	
+	public static BufferedImage start;
+	
+	public static BufferedImage over;
+		
+	public static void initialize(ImageLoader imageLoader) {
+		day=imageLoader.loadImage("background/day.png");
+		
+		night=imageLoader.loadImage("background/night.png");
+		
+		start=imageLoader.loadImage("background/start.png");
+		over=imageLoader.loadImage("background/over.png");
+	}
+}
