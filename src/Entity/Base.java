ba46bc563e02caf43b30c20e18155edd17ea7206
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-07-16 11:58:42 +0800

    Update Base.java

--- src/Entity/Base.java
+++ src/Entity/Base.java
@@ -1,1 +1,34 @@
+package Entity;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import Asset.Obstacle;
+import Main.Display;
+import State.State;
+
+public class Base extends Entity {
+	
+	private final BufferedImage base=Obstacle.base;
+	
+	public Base(Display display,int x,int y,int width,int height) {
+		super(display,x,y,width,height);
+	}
+	
+	public void tick() {
+		if(x>=0) {
+			x=-display.getWidth();
+		}else {
+			x+=6;
+		}
+		
+		if(display.getGameState().getBird().y+display.getGameState().getBird().height>=this.y) {
+			State.setState(display.getStartState());
+			display.restart();
+		}
+	}
+	
+	public void render(Graphics2D g) {
+		g.drawImage(base,x,y,width,height,null);
+	}
+}
