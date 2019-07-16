babdf01f24464a2b986992708acf702c1e5097bd
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-07-16 12:03:25 +0800

    Update Bird.java

--- src/Asset/Bird.java
+++ src/Asset/Bird.java
@@ -1,1 +1,21 @@
+package Asset;
 
+import java.awt.image.BufferedImage;
+
+import Loader.ImageLoader;
+
+public class Bird {
+	public static BufferedImage downFlap;
+	
+	public static BufferedImage midFlap;
+	
+	public static BufferedImage upFlap;
+	
+	public static void initialize(ImageLoader imageLoader) {
+		downFlap=imageLoader.loadImage("bird/downFlap.png");
+		
+		midFlap=imageLoader.loadImage("bird/midFlap.png");
+		
+		upFlap=imageLoader.loadImage("bird/upFlap.png");
+	}
+}
