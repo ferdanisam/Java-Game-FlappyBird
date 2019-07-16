497f4efc56e7cd1d3263b6b95973ff0e2caf6429
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-07-16 12:04:55 +0800

    Update Obstacle.java

--- src/Asset/Obstacle.java
+++ src/Asset/Obstacle.java
@@ -1,1 +1,21 @@
+package Asset;
 
+import java.awt.image.BufferedImage;
+
+import Loader.ImageLoader;
+
+public class Obstacle {
+	public static BufferedImage base;
+	
+	public static BufferedImage pipeUp;
+	
+	public static BufferedImage pipeDown;
+	
+	public static void initialize(ImageLoader imageLoader) {
+		base=imageLoader.loadImage("obstacle/base.png");
+		
+		pipeUp=imageLoader.loadImage("obstacle/pipeUp.png");
+		
+		pipeDown=imageLoader.loadImage("obstacle/pipeDown.png");
+	}
+}
